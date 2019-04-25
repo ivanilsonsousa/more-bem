d0949a0b38adb1d1b0390f7e3dd396c95af44157
Ivanilson Sousa de Oliveira <ivanilsonsousa95@gmail.com>
2019-04-25 19:57:00 -0300

    criacao e estilizacao das telas gerenciar usuarios e gerenciar funcionarios do (adm)

--- tela-adm-gerenciar-fornecedores.php
+++ tela-adm-gerenciar-fornecedores.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <title>Gerenciar Fornecedores</title>
-  </head>
-  <body>
-    <a href="tela-ver-fornecedores.php">Ver Fornecedores</a>
-    <a href="tela-novo-fornecedor.php">Adicionar Fornecedor</a>
-    <a href="tela-validar-fornecedor.php">Validar Fornecedor</a>
-  </body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Gerenciar Fornecedores</title>
+		<link rel="shortcut icon" href="_imagens/icone.png">
+		<link rel="stylesheet" type='text/css' href="_css/estilo-tela-gerenciar-fornecedores-adm.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
+		<link rel="stylesheet" type='text/css' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+	</head>
+	<body>
+
+		<div class="site">
+      <i class="fas fa-cogs fa-4x"></i><i class="fas fa-address-card fa-2x"></i><br><br>
+			<fieldset id="form-content">
+				<legend>Gerenciar Fornecedores</legend><br>
+        <div class="barra-pesquisa">
+        </div>
+        <div id="div-pesquisa" class="blocoIcones">
+          <input class="blocoIcones" type="text" placeholder="Pesquisar...">
+          <button class="blocoIcones"><i class="fas fa-search"></i></button>
+        </div>
+        <div onclick="window.location.href='tela-validar-fornecedor.php';" class="botao">
+          <i class="fas fa-suitcase"></i>
+          <label>Validar Fornecedores</label>
+        </div>
+
+        <a href="tela-ver-fornecedores.php">Ver Fornecedores</a>
+        <a href="tela-novo-fornecedor.php">Adicionar Fornecedor</a>
+        <a href="tela-validar-fornecedor.php">Validar Fornecedor</a>
+			</fieldset>
+		</div>
+
+	</body>
 </html>
--- tela-adm-gerenciar-usuarios.php
+++ tela-adm-gerenciar-usuarios.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <title>Gerenciar Usuários</title>
-  </head>
-  <body>
-    <a href="tela-ver-usuarios.php">Ver Usuarios</a>
-    <a href="tela-novo-usuario.php">Adicionar Usuario</a>
-  </body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Gerenciar Usuários</title>
+		<link rel="shortcut icon" href="_imagens/icone.png">
+		<link rel="stylesheet" type='text/css' href="_css/estilo-tela-gerenciar-usuarios-adm.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
+		<link rel="stylesheet" type='text/css' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+	</head>
+	<body>
+
+		<div class="site">
+      <i class="fas fa-cogs fa-4x"></i><i class="fas fa-address-card fa-2x"></i><br><br>
+			<fieldset id="form-content">
+				<legend>Gerenciar Usuários</legend><br>
+        <div class="barra-pesquisa">
+          <i id="mais" onclick="window.location.href='tela-novo-usuario.php';" class="fas fa-plus-circle fa-2x"></i>
+        </div>
+        <div id="div-pesquisa" class="blocoIcones">
+          <input class="blocoIcones" type="text" placeholder="Pesquisar...">
+          <button class="blocoIcones"><i class="fas fa-search"></i></button>
+        </div>
+        <div class="botao">
+          <i class="fas fa-address-book"></i>
+          <label>Meu Perfil</label>
+        </div>
+
+        <!-- <a href="tela-ver-usuarios.php">Ver Usuarios</a> -->
+        <!-- <a href="tela-novo-usuario.php">Adicionar Usuario</a> -->
+			</fieldset>
+		</div>
+
+	</body>
 </html>
