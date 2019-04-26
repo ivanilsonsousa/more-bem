a42759761fc91b5f9b08ed32e9beb0e433e878c1
Ivanilson Sousa de Oliveira <ivanilsonsousa95@gmail.com>
2019-04-26 18:31:44 -0300

    estilizacao da tela novo usuario

--- tela-novo-usuario.php
+++ tela-novo-usuario.php
@@ -3,33 +3,40 @@
 <head>
 	<title>Cadastrar Usuário</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="_css/estilo-tela-novo-user.css">
+	<link rel="stylesheet" href="_css/estilo-tela-novo-usuario.css">
 	<link rel="shortcut icon" href="_imagens/icone.png">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
+	<link rel="stylesheet" type='text/css' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
-	<fieldset id="campo-cadastro">
-	<legend id="legenda">Cadastrar Usuário</legend>
-	<form method="POST" action="_controles/processa-cadastro-fornecedor.php">
+	<div class="site">
+		<i class="fas fa-user-plus fa-4x"></i><br><br>
+		<fieldset id="campo-cadastro">
+		<legend id="legenda">Cadastrar Usuário</legend>
+		<form method="POST" action="_controles/processa-cadastro-fornecedor.php">
 
-		<label for="nome">Nome:</label> <input type="text" id="nome" name="nome"><br>
-		<label for="cnpj">CPF:</label> <input type="text" id="cpf" name="cpf"><br>
+			<label for="nome">Nome:</label> <input type="text" id="nome" name="nome"><br>
+			<label for="cnpj">CPF:</label> <input type="text" id="cpf" name="cpf"><br>
 
-		<fieldset id="tipo-user" class="form-interno, relative-parent">
-		<legend id="legenda-end">Tipo de Usuário</legend>
-			<div id="tipos" class="relative">
-				<input type="radio" name="tipo" id="adm" value="0" checked><label id="label-adm" for="adm"> Administrador</label>
-				<input type="radio" name="tipo" id="pad" value="1"><label id="label-pad" for="pad"> Padrão</label>
-			</div>
+			<fieldset class="form-interno">
+			<legend id="legenda-end">Tipo de Usuário</legend>
+				<div class="tipos">
+					<input type="radio" name="tipo" id="adm" value="0"><label id="label-adm" for="adm"> Administrador</label>
+					<input type="radio" name="tipo" id="pad" value="1" checked><label id="label-pad" for="pad"> Padrão</label>
+				</div>
+			</fieldset>
+
+			<fieldset class="form-interno">
+			<legend id="legenda-dados-login">Dados de Login</legend>
+				<label for="email">Email:</label><input type="email" id="email" name="email"><br>
+				<label for="senha">Senha:</label><input type="password" id="senha" name="senha"><br>
+				<label for="rsenha">Repetir Senha:</label><input type="password" id="rsenha" name="rsenha"><br>
+			</fieldset>
+			<input class="btn btn-default" type="submit" value="Cadastrar" id="botao"><br>
+		</form>
 		</fieldset>
-
-		<fieldset id="dados-login" class="form-interno">
-		<legend id="legenda-dados-login">Dados de Login</legend>
-			<label for="email">Email:</label><input type="email" id="email" name="email"><br>
-			<label for="senha">Senha:</label><input type="password" id="senha" name="senha"><br>
-			<label for="rsenha">Repetir Senha:</label><input type="password" id="rsenha" name="rsenha"><br>
-		</fieldset>
-		<input type="submit" value="Cadastrar" id="botao"><br>
-	</form>
-	</fieldset>
+	</div>
 </body>
 </html>
